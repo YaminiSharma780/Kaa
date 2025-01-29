b3a0a747db9fa687aff9cc2a31f8a58f9551eaf7
Yamini <sharmayamini1999@gmail.com>
2025-01-30 01:19:15 +0530

    added snake & food collision

--- script.js
+++ script.js
@@ -37,28 +37,55 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   function gameLoop() {
     setInterval(() => {
+      updateSnakeCoordinates();
       drawScoreBoard();
       drawFoodAndSnake();
     }, 1000);
+  }
+  function updateSnakeCoordinates() {
+    const newSnakeHead = { x: snake[0].x + dx, y: snake[0].y + dy };
+    snake.unshift(newSnakeHead); // add new cell in start
+    if (newSnakeHead.x === food.x && newSnakeHead.y == food.y) {
+      // snake food collision
+      score += 5;
+      moveFood();
+    } else {
+      snake.pop(); // remove last cell from end
+    }
+  }
+  function moveFood() {
+    let newX, newY;
+    do {
+      newX = Math.floor(
+        Math.random() * ((arenaSize - cellSize) / cellSize) * cellSize
+      );
+      newY = Math.floor(
+        Math.random() * ((arenaSize - cellSize) / cellSize) * cellSize
+      );
+    } while (snake.some((snakeCell) => snakeCell.x === newX && snakeCell.y === newY));
+    food = { x: newX, y: newY };
   }
   function drawScoreBoard() {
     const scoreBoard = document.getElementById("score-board");
     scoreBoard.textContent = `Score : ${score}`;
   }
-  function drawFoodAndSnake(){
-    gameArena.innerHTML = '';
+  function drawFoodAndSnake() {
+    gameArena.innerHTML = "";
 
-    const foodElement = drawDiv(food.x, food.y, 'food');
+    const foodElement = drawDiv(food.x, food.y, "food");
     gameArena.appendChild(foodElement);
 
-    const snakeElement = drawDiv(snake.x, snake.y, 'snake');
-    gameArena.appendChild(snakeElement);
+    snake.forEach((snakeIterator) => {
+      console.log(snakeIterator.x, snakeIterator.y);
+      const snakeElement = drawDiv(snakeIterator.x, snakeIterator.y, "snake");
+      gameArena.appendChild(snakeElement);
+    });
   }
-  function drawDiv(x, y, className){
-    const div = document.createElement('div');
+  function drawDiv(x, y, className) {
+    const div = document.createElement("div");
     div.classList.add(className);
-    div.style.top = `${y}px`
-    div.style.left = `${x}px`
+    div.style.top = `${y}px`;
+    div.style.left = `${x}px`;
     return div;
   }
 });
