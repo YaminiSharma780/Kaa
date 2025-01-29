d8ba3e8f63c3fa8eda3ebf8dd71007e2af06a10f
Yamini <sharmayamini1999@gmail.com>
2025-01-30 00:25:14 +0530

    added game arena for kaa

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gameArena = document.getElementById("game-arena");
+  const arenaSize = 500;
+  const cellSize = 20;
+  let score = 0;
+  let gameStarted = false;
+  let food = { x: 300, y: 200 };
+  let snake = [
+    { x: 160, y: 200 },
+    { x: 140, y: 200 },
+    { x: 120, y: 200 },
+  ];
+  let dx = cellSize;
+  let dy = 0;
+
+  function startGame() {
+    const scoreBoard = document.createElement("div");
+    scoreBoard.id = "score-board";
+    scoreBoard.textContent = "10";
+    document.body.insertBefore(scoreBoard, gameArena);
+
+    const startButton = document.createElement("button");
+    startButton.id = "start-button";
+    startButton.textContent = "Start";
+    startButton.classList.add("start-button");
+    document.body.appendChild(startButton);
+  }
+  startGame();
+});
